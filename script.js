7c58a36c4e65da8ff2548e908a5dd24840f5f77d
havcodls <160694522+havcodls@users.noreply.github.com>
2024-02-25 05:37:45 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+window.addEventListener('scroll', function(){
+    let navbar = document.getElementById('navbar');
+    let sec1 = document.getElementById('sec1');
+    let sec1Top = sec1.offsetTop;
+
+    if (window.pageYOffset >= sec1Top) {
+        navbar.classList.add('black-navbar');
+    } else {
+        navbar.classList.remove('black-navbar');
+    }
+});
+
+
