25eed8723dfb0d276f47cad5f3c088e30193c957
havcodls <160694522+havcodls@users.noreply.github.com>
2024-02-25 07:41:15 +0100

    script.js

--- script.js
+++ script.js
@@ -10,4 +10,8 @@ window.addEventListener('scroll', function(){
     }
 });
 
+const hamburger = document.querySelector('.hamburger');
 
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('active');
+});
